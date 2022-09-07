0264fcd4e6c2eb6f142581f4df71468209f67708
Diego Pereira <diego.pereira@prosoft.com.br>
2022-09-07 18:02:15 -0300

    Utilizando IAuthorizantioService para validar se o usuario possui uma determinada claim para que seja executada uma logica especifica dentro de um determinado metodo

--- BasicAuthNew/Controllers/HomeController.cs
+++ BasicAuthNew/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BasicAuth.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IAuthorizationService _authorizationService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IAuthorizationService authorizationService)
         {
             _logger = logger;
+            _authorizationService = authorizationService;
         }
 
         public IActionResult Index()
@@ -50,6 +55,17 @@ namespace BasicAuth.Controllers
             HttpContext.SignInAsync(userPrincipal); 
 
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DoStuff()
+        {
+            var authResult = await _authorizationService.AuthorizeAsync(HttpContext.User, "Claim.DoB");
+
+            if (authResult.Succeeded)
+            {
+                //executa alguma lógica específica
+            }
+            return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
