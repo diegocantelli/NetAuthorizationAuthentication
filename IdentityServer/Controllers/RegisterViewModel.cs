4e6f7178d26ecfdd7c1c15a13e96e303fd6b1813
Diego Pereira <diego.pereira@prosoft.com.br>
2022-09-17 13:49:30 -0300

    Adicionando data annotations na registerViewModel para fazer a validação se as senhas são iguais

--- IdentityServer/Controllers/RegisterViewModel.cs
+++ IdentityServer/Controllers/RegisterViewModel.cs
@@ -1,9 +1,18 @@
-﻿namespace IdentityServer.Controllers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer.Controllers
 {
     public class RegisterViewModel
     {
         public string Username { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
         public string ReturnUrl { get; set; }
     }
