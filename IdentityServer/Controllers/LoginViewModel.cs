4e33da2450c5960d55252de3583e4612c4a31ab2
Diego Pereira <diego.pereira@prosoft.com.br>
2022-09-17 11:11:56 -0300

    Criando AuthController no projeto do servidor de identidade

---
+++ IdentityServer/Controllers/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityServer.Controllers
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
