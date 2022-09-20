27363a9719da521b93f1c7755d58d16247f2c9fd
Diego Pereira <diego.pereira@prosoft.com.br>
2022-09-20 17:41:55 -0300

    Montando a url que deverá ser redirecionada para o identity no sign-in.js

--- JavaScriptClient/wwwroot/sign-in.js
+++ JavaScriptClient/wwwroot/sign-in.js
@@ -1,1 +1,26 @@
-﻿
+﻿var createState = function () {
+    return "SessionValueSADJKBKJBDKASANSJADKJBAKDJB";
+}
+
+var createNonce = function () {
+    return "NonceValueHSASJHASJKHAJKSHAJKSHJKADAJBDJDJADHSSAJKOIOUDOID";
+}
+
+
+
+var signIn = function () {
+    var redirectUri = "https://localhost:5080/signin";
+    var responseType = "id_token token";
+    var scope = "openid ApiOne";
+    var authUrl = `/connect/authorize/callback
+                   ?client_id=client_id_js
+                   ?redirect_uri=${encodeURIComponent(redirectUri)}
+                   ?response_type=${encodeURIComponent(responseType)}
+                   ?scope=${encodeURIComponent(scope)}
+                   ?nonce=${createNonce()} 
+                   ?session=${createState()}`
+
+    var returnUrl = encodeURIComponent(authUrl);
+
+    window.location.href = "https://localhost:5010/Auth/Login?ReturnUrl=" + returnUrl;
+}
