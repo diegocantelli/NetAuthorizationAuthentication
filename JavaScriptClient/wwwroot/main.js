8510f5427aa145387c46ab29840f90fbde5c6f48
Diego Pereira <diego.pereira@prosoft.com.br>
2022-09-22 10:37:55 -0300

    Configurando e referenciando os arquivos js corretamente

---
+++ JavaScriptClient/wwwroot/main.js
@@ -0,0 +1,9 @@
+﻿var config = {
+    authority: "https://localhost:5010/",
+    client_id: "client_id_js",
+    redirect_uri: "https://localhost:5080/home/signin",
+    response_type: "id_token token",
+    scope: "openid ApiOne"
+};
+
+var userManager = new Oidc.UserManager(config);
